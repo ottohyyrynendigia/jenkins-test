61402b66dce4dcc441d6911ab7d42a731b33e503
Otto Hyyrynen <otto.hyyrynen@digia.com>
2023-12-28 15:46:22 +0200

    dasd

--- env/buildenv-local.sh
+++ env/buildenv-local.sh
@@ -1,3 +1,5 @@
 # Defines environment variables for building the UI for dev environment
 export REACT_APP_ALERTS_API_URL=http://localhost:8080
 export REACT_APP_ENV=local
+
+echo "buildenv-local.sh ran"
